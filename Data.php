e7797faa51effa7f02c49caf8a8ae82be8c5d7b4
Dreams <861683052@qq.com>
2023-03-02 23:19:22 +0800

    Update Data.php

--- Data.php
+++ Data.php
@@ -7,3 +7,33 @@
 
 $Output_format=@$_SERVER['format'] ?: $_SERVER['type'];
 $Output_callback=@$_SERVER['callback'];
+
+
+	/**	 * 获取用户端IP
+	 *
+	 * @return string
+	 */
+public static function get_ip()
+{
+$unknown='unknown';
+    if(isset($_SERVER['HTTP_X_FORWARDED_FOR']) && $_SERVER['HTTP_X_FORWARDED_FOR'] && strcasecmp($_SERVER['HTTP_X_FORWARDED_FOR'],$unknown))
+		{
+		$ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+		}
+		else if(isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'],$unknown))
+		{
+		$ip=@$_SERVER['REMOTE_ADDR'];
+		}
+		return $ip;
+    }
+}
+
+/**
+ * 随机IP
+ *
+ * @return string
+ */
+function rand_ip()
+{
+    return mt_rand(0,255).' . '.mt_rand(0,255).' . '.mt_rand(0,255).' . '.mt_rand(0,255);
+}
